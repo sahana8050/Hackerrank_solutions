c02dd853488a5ca33a104961bd200e1358fcb932
Sahana <sahanar8050@gmail.com>
2026-03-10 18:18:28 +0530

    division

---
+++ arthematic.py
@@ -0,0 +1,8 @@
+a = int(input("Enter value of a"))
+b = int(input("Enter value of b"))
+s=a+b 
+n=a-b 
+d=a*b
+print(s)
+print(n)
+print(d)
---
+++ division.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter n1 value"))
+n2=int(input("Enter n2 value"))
+a=n1//n2
+b=n1/n2
+print(a)
+print(b)
